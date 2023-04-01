d2b1ef3bbb57a1ce92ba37b4b64774fa6736f873
Rensie Dela Cruz <rensie.delacruz1@gmail.com>
2023-04-01 13:31:27 -0500

    Added eventlistener on change-resolution and reset buttons

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,36 @@
-createGrid();
+const container = document.querySelector('.container');
+let size = 16;
+createGrid(size);
 
-function createGrid() {
-    const container = document.querySelector('.container');
-    let size = 100;
+const changeResolutionButton = document.querySelector('.resolution-btn');
+changeResolutionButton.addEventListener('click', function () {
+    newSize = parseInt(prompt('Enter a grid size(limit of 100): '));
+
+    if (newSize > 100) {
+        alert("Maximum allowed is 100");
+    } else if (newSize <= 0) {
+        alert("Grid size cannot be 0");
+    } else if (newSize > 0 && newSize <= 100) {
+        const rows = document.querySelectorAll('.row');
+        rows.forEach((row) => {
+            container.removeChild(row);
+        });
+        createGrid(newSize);
+    } else {
+        alert("Please enter a valid number!");
+    }
+});
+
+const resetButton = document.querySelector('.reset-btn');
+resetButton.addEventListener('click', function () {
+    const filledSquares = document.querySelectorAll(".row-square.filled");
+    filledSquares.forEach((square) => {
+        square.classList.remove('filled');
+    });
+});
+
+
+function createGrid(size) {
 
     for (let col = 0; col < size; col++) {
         const rowContainer = document.createElement('div');
@@ -15,13 +43,17 @@ function createGrid() {
         }
         container.appendChild(rowContainer);
     }
+
+    addFillEffect();
 }
 
-const squares = document.querySelectorAll('.row-square');
-squares.forEach((square) => {
+function addFillEffect() {
+    const squares = document.querySelectorAll('.row-square');
+    squares.forEach((square) => {
 
-    square.addEventListener('mouseenter', function () {
-        square.classList.add('filled');
+        square.addEventListener('mouseenter', function () {
+            square.classList.add('filled');
+        });
+
     });
-
-});
+}
