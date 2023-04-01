b01a51b63ce1c6bcb8c8a5cb541bf06c2a2e1694
Rensie Dela Cruz <rensie.delacruz1@gmail.com>
2023-04-01 12:09:30 -0500

    Created the hovering effect that fills squares

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,12 +2,12 @@ createGrid();
 
 function createGrid() {
     const container = document.querySelector('.container');
-    let size = 16;
+    let size = 100;
 
     for (let col = 0; col < size; col++) {
         const rowContainer = document.createElement('div');
         rowContainer.classList.add('row');
-        
+
         for (let row = 0; row < size; row++) {
             const square = document.createElement('div');
             square.classList.add('row-square');
@@ -16,3 +16,12 @@ function createGrid() {
         container.appendChild(rowContainer);
     }
 }
+
+const squares = document.querySelectorAll('.row-square');
+squares.forEach((square) => {
+
+    square.addEventListener('mouseenter', function () {
+        square.classList.add('filled');
+    });
+
+});
